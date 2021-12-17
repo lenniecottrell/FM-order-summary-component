5e4bff85fc304325346c75b358e8dbf15235872d
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2021-12-17 10:17:28 -0800

    revisions based on FM feedback

--- order-summary-component/src/components/Card.js
+++ order-summary-component/src/components/Card.js
@@ -7,8 +7,9 @@ import heroSVG from '../images/illustration-hero.svg'
 
 const Card = () => {
     return (
-        <div className="card">
-            <img className="heroImage" src={heroSVG} alt="heroImage" />
+        <main className="card">
+            <h1 className="sr-only">Order Summary Card Component</h1>
+            <img className="heroImage" src={heroSVG} alt="" aria-hidden="true" />
             <div className="cardContent">
                 <h2>Order Summary</h2>
                 <p>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
@@ -16,7 +17,7 @@ const Card = () => {
                 <Proceed />
                 <a href="#" className="cancelOrder">Cancel Order</a>
             </div>
-        </div>
+        </main>
     )
 }
 
--- order-summary-component/src/components/PlanSelection.js
+++ order-summary-component/src/components/PlanSelection.js
@@ -4,9 +4,9 @@ import { ReactComponent as MusicIcon } from '../images/icon-music.svg'
 const PlanSelection = () => {
     return (
         <div className = "planSelection">
-            <MusicIcon className="musicIcon"/>
+            <MusicIcon className="musicIcon" aria-hidden="true"/>
             <div className="planText">
-                <h4 className="planType">Annual Plan</h4>
+                <h3 className="planType">Annual Plan</h3>
                 <p className="price">$59.99/year</p>
             </div>
             <a href='#' className="changeLink"> Change</a>
